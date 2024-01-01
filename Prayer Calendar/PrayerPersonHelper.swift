7e3388110a518647d7fd8d8d569f5a5ce3acbfea
lammylol <44993071+lammylol@users.noreply.github.com>
2023-12-31 22:54:30 -0800

    Updates to split firstname and username
    Before: Username would be filled in as first name if did not exist
    Now: Username will be empty if doesn't exist.

--- Prayer Calendar/PrayerPersonHelper.swift
+++ Prayer Calendar/PrayerPersonHelper.swift
@@ -41,14 +41,30 @@ class PrayerPersonHelper {
     func retrievePrayerPersonArray(prayerList: String) -> [PrayerPerson] {
         let ref = prayerList.components(separatedBy: "\n")
         var prayerArray: [PrayerPerson] = []
+        var firstName = ""
+        var lastName = ""
+        var username = ""
         
         for person in ref {
             let array = person.split(separator: ";", omittingEmptySubsequences: true) // Separate name from username
-            let nameArray = array.first?.split(separator: " ", omittingEmptySubsequences: true) // Separate First name from Last. If there is middle name, last name will only grab last of array.
-            let firstName = String(nameArray?.first ?? "").trimmingCharacters(in: .whitespaces)
-            let lastName = String(nameArray?.last ?? "").trimmingCharacters(in: .whitespaces)
             
-            let prayerPerson = PrayerPerson(username: String(array.last ?? "").trimmingCharacters(in: .whitespaces), firstName: firstName, lastName: lastName)
+            if array.count == 1 {
+                username = "" // If the array is only 1, assume that there was no username entered.
+            } else {
+                username = String(array.last ?? "").trimmingCharacters(in: .whitespaces)
+            }
+            
+            let nameArray = array.first?.split(separator: " ", omittingEmptySubsequences: true) // Separate First name from Last. If there is middle name, last name will only grab last of array.
+            
+            // nameArray?.count == 1 ensures that if user enters only first name, last name will be "", not first name.
+            if nameArray?.count == 1 {
+                firstName = String(nameArray?.first ?? "").trimmingCharacters(in: .whitespaces)
+            } else {
+                firstName = String(nameArray?.first ?? "").trimmingCharacters(in: .whitespaces)
+                lastName = String(nameArray?.last ?? "").trimmingCharacters(in: .whitespaces)
+            }
+            
+            let prayerPerson = PrayerPerson(username: username, firstName: firstName, lastName: lastName)
             prayerArray.append(prayerPerson)
         }
         
@@ -56,23 +72,27 @@ class PrayerPersonHelper {
     }
     
     // Retrieve requested userID off of username
-    func retrieveUserID(person: PrayerPerson) async -> String {
+    func retrieveUserID(person: PrayerPerson,userHolder: UserProfileHolder) async -> String {
         var userID = ""
         let db = Firestore.firestore()
         let ref = db.collection("usernames").document(person.username)
-
-        do {
-            let document = try await ref.getDocument()
-            if document.exists {
-                let dataDescription = document.data().map(String.init(describing:)) ?? "nil"
-                print("Document data: " + dataDescription)
-                
-                userID = document.get("userID") as! String
-            } else {
-                print("Error Retrieving User ID. Document does not exist")
+        
+        if person.username == userHolder.person.username {
+            userID = userHolder.person.userID
+        } else {
+            do {
+                let document = try await ref.getDocument()
+                if document.exists {
+                    let dataDescription = document.data().map(String.init(describing:)) ?? "nil"
+                    print("Document data: " + dataDescription)
+                    
+                    userID = document.get("userID") as! String
+                } else {
+                    print("Error Retrieving User ID. Document does not exist")
+                }
+            } catch {
+                print("Error getting document: \(error)")
             }
-        } catch {
-            print("Error getting document: \(error)")
         }
         
         print("username: \(person.username); userID: \(userID)")
